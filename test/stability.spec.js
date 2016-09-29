809263888088972f72d1ecf29395fc073bd4b059
Benjamin Albert <benjamin_a5@yahoo.com>
2016-09-29 14:36:19 -0400

    Adds server created by user that was NOT intercepted works as expected to stability.spec.js

--- test/stability.spec.js
+++ test/stability.spec.js
@@ -3,21 +3,55 @@ var http = require('http');
 var request = require('superagent');
 var intercept = require('../');
 
+var PORT = 42653;
+
 test('server that was NOT intercepted works as expected', function(t) {
   t.plan(1);
 
-  var port = 42653;
-  var io = require('socket.io')(port);
+  PORT++;
+
+  var io = require('socket.io')(PORT);
 
   // The point of this test is to make sure that we are
   // NOT mocking socket.io (and http server's listen)
   // so we make a real HTTP request.
   request
-    .get('http://localhost:' + port)
+    .get('http://localhost:' + PORT)
     .end(function(err, res) {
       t.equal(res.status, 404, 'The expected status wes returnd');
 
-      var client = require('socket.io-client')('http://localhost:' + port);
+      var client = require('socket.io-client')('http://localhost:' + PORT);
+      client.on('connect', function() {
+        client.disconnect();
+        io.close();
+        t.end();
+      })
+    });
+});
+
+test('server created by user that was NOT intercepted works as expected', function(t) {
+  t.plan(1);
+
+  PORT++;
+
+  var server = http.createServer(function(req, res) {
+    res.writeHead(404);
+    res.end();
+  });
+
+  var io = require('socket.io')(server);
+
+  server.listen(PORT);
+
+  // The point of this test is to make sure that we are
+  // NOT mocking socket.io (and http server's listen)
+  // so we make a real HTTP request.
+  request
+    .get('http://localhost:' + PORT)
+    .end(function(err, res) {
+      t.equal(res.status, 404, 'The expected status wes returnd');
+
+      var client = require('socket.io-client')('http://localhost:' + PORT);
       client.on('connect', function() {
         client.disconnect();
         io.close();
